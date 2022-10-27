7273902137368c0a7ecdf66d96ee84e45087189d
Patrick M. Ryan <patrick.ryan@evanschambers.com>
2022-10-27 16:14:19 -0400

    able to deploy web servers

--- asg/asg_stack.py
+++ asg/asg_stack.py
@@ -16,6 +16,8 @@ class AsgStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        self.ec2_resource = boto3.resource("ec2")
+
         # get the VPC
         vpc_id = self.node.try_get_context("VpcId")
         vpc = ec2.Vpc.from_lookup(self, "Vpc", vpc_id=vpc_id)
@@ -43,7 +45,7 @@ class AsgStack(Stack):
 yum update -y
 yum install httpd -y
 name=$(curl http://169.254.169.254/latest/meta-data/local-hostname)
-cat > /var/www/html/index.htm<<__EOF__
+cat > /var/www/html/index.html <<__EOF__
 <title>$name</title>
 <h1>welcome to $name</h1>
 __EOF__
@@ -62,30 +64,21 @@ systemctl start httpd
                 ec2.InstanceClass.T3, ec2.InstanceSize.MICRO
             ),
             key_name=key_name,
-            machine_image=ec2.MachineImage.latest_amazon_linux(),  #'ami-09d3b3274b6c5d4aa'
+            machine_image=ec2.AmazonLinuxImage(
+                generation=ec2.AmazonLinuxGeneration.AMAZON_LINUX_2
+            ),
+            # machine_image=ec2.MachineImage.lookup("amzn2-ami-kernel-*"),
+            # filters={"image-id": "ami-09d3b3274b6c5d4aa"}
             role=instance_role,
             security_group=unrestricted_sg,
             user_data=user_data,
         )
 
         # vpc_subnets
-        tag_key = "CWALL_ROLE"
-        tag_value = "ECHO"
+        vpc_resource = self.ec2_resource.Vpc(vpc_id)
 
-        ec2_resource = boto3.resource("ec2")
-        vpc_resource = ec2_resource.Vpc(vpc_id)
-        all_subnets = vpc_resource.subnets.all()
-        subnet_ids = []
-        for subnet in all_subnets:
-            tags = {tag["Key"]: tag["Value"] for tag in subnet.tags}  # dict-ify
-            if tags[tag_key] == tag_value:
-                subnet_ids.append(subnet.subnet_id)
-
-        vpc_subnets = ec2.SubnetSelection(
-            subnets=[
-                ec2.Subnet.from_subnet_id(self, "Subnet-" + subnet_id, subnet_id)
-                for subnet_id in subnet_ids
-            ]
+        subnets = self.get_subnets_tagged(
+            vpc=vpc_resource, tag_key="CWALL_ROLE", tag_value="ECHO"
         )
 
         # ASG
@@ -97,13 +90,53 @@ systemctl start httpd
             # associate_public_ip_address
             # group_metrics
             # health_check
+            # instance_monitoring
             min_capacity=0,
+            max_capacity=4,
             # notifications
-            vpc_subnets=vpc_subnets,
+            # termination_policies
+            # update_policy
+            vpc_subnets=ec2.SubnetSelection(subnets=subnets),
         )
 
         # ALB
-        # target group
+        subnets = self.get_subnets_tagged(
+            vpc=vpc_resource, tag_key="CWALL_ROLE", tag_value="EGRESS"
+        )
+
+        alb = elbv2.ApplicationLoadBalancer(
+            self,
+            "ALB",
+            vpc=vpc,
+            vpc_subnets=ec2.SubnetSelection(subnets=subnets),
+            internet_facing=True,
+        )
+        listener = alb.add_listener(
+            "Listener",
+            port=80,
+            # 'open: true' is the default, you can leave it out if you want. Set it
+            # to 'false' and use `listener.connections` if you want to be selective
+            # about who can access the load balancer.
+            open=True,
+        )
+        listener.add_targets("WebServers", port=80, targets=[asg])
+
         # scaling topic(s)
+        # https://docs.aws.amazon.com/cdk/api/v2/python/aws_cdk.aws_autoscaling/LifecycleHook.html
+
         # event rules
         # lambdas attached to rules(super basic)
+
+    def get_subnets_tagged(self, vpc=None, tag_key=None, tag_value=None):
+
+        subnets = []
+        for subnet in vpc.subnets.all():
+            tags = {tag["Key"]: tag["Value"] for tag in subnet.tags}  # dict-ify
+            if tags[tag_key] == tag_value:
+                subnets.append(
+                    ec2.Subnet.from_subnet_id(
+                        self, "Subnet-" + subnet.subnet_id, subnet.subnet_id
+                    )
+                )
+
+        return subnets
